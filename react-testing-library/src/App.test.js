1b8f950c658796f490899331213e7c0b65928e24
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-12-31 01:18:12 -0800

    Separated text tests into separate tests

--- react-testing-library/src/App.test.js
+++ react-testing-library/src/App.test.js
@@ -3,12 +3,26 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
-  test('renders App component', () => {
+describe('renders App component', () => {
+
+  // test('Displays html', () => {
+  //   render(<App />);
+  //   screen.debug()
+  // });
+
+  test('Exact match of string without last character', () => {
     render(<App />);
-    //screen.debug() // console.log
-    expect(screen.getByText('Search')).toBeInTheDocument() // Exact string match = fails
-    expect(screen.getByText('Search:')).toBeInTheDocument() // Exact string match = succeeds
-    expect(screen.getByText(/Search/)).toBeInTheDocument() // Regex partial match = succeeds
+    expect(screen.getByText('Search')).toBeInTheDocument()
+  });
+  
+  test('Exact match of string', () => {
+    render(<App />);
+    expect(screen.getByText('Search:')).toBeInTheDocument()
+  });
+  
+  // Whereas a string argument is used for the exact match, a regular expression can be used for a partial match which is often more convenient
+  test('Partial string match using regex', () => {
+    render(<App />);
+    expect(screen.getAllByText(/Search/)).toHaveLength(2)
   });
 });
