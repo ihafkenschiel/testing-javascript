5535f501b6fb54e9f78e05b10aae59bf83148921
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-12-31 01:40:05 -0800

    Successfully check if text is not there

--- react-testing-library/src/App.test.js
+++ react-testing-library/src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-describe("renders App component", () => {
+describe("Test search types", () => {
   // test('Displays html', () => {
   //   render(<App />);
   //   screen.debug()
@@ -14,13 +14,13 @@ describe("renders App component", () => {
   //   expect(screen.getByText('Search')).toBeInTheDocument()
   // }); // Fails
 
-  test("Exact match of string", () => {
+  test("Exact match of string (getByText)", () => {
     render(<App />);
     expect(screen.getByText("Search:")).toBeInTheDocument();
   });
 
   // Whereas a string argument is used for the exact match, a regular expression can be used for a partial match which is often more convenient
-  test("Partial string match using regex", () => {
+  test("Partial string match using regex (getAllByText)", () => {
     render(<App />);
     expect(screen.getAllByText(/Search/)).toHaveLength(2);
   });
@@ -31,33 +31,47 @@ describe("renders App component", () => {
   //   screen.getByRole('');
   // })
 
-  test("Exists an input element (by role)", () => {
+  test("Exists an input element (getByRole)", () => {
     render(<App />);
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
-  test("Exists a label element (by Label Text)", () => {
+  test("Exists a label element (getByLabelText)", () => {
     render(<App />);
     expect(screen.getByLabelText("Search:")).toBeInTheDocument();
   });
 
-  test("Exists placeholder text", () => {
+  test("Exists placeholder text (getByPlaceholderText)", () => {
     render(<App />);
     expect(screen.getByPlaceholderText("Search...")).toBeInTheDocument();
   });
 
-  test("Exists image (by Alt Text)", () => {
+  test("Exists image (getByAltText)", () => {
     render(<App />);
     expect(screen.getByAltText("profile")).toBeInTheDocument();
   });
 
-  test("Exists input (by Display Value)", () => {
+  test("Exists input (getByDisplayValue)", () => {
     render(<App />);
     expect(screen.getByDisplayValue("")).toBeInTheDocument();
   });
 
-  test("Exists input (by Test ID)", () => {
+  test("Exists input (getByTestId)", () => {
     render(<App />);
     expect(screen.getByTestId("search-result")).toBeInTheDocument();
   });
 });
+
+describe('Test search variants', () => {
+  
+  // test('Get by throws error if element not there', () => {
+  //   render(<App />)
+  //   expect(screen.getByText(/Searches for JavaScript/)).toBeNull();
+  // }) // Fails
+
+  // Use queryBy when element shouldn't be there
+  test('Text element is not there (queryByText)', () => {
+    render(<App />)
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  }) 
+})
