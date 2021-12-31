19402c85a6aa1907c12eaaa54bc5d8a98a9d6968
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-12-31 01:22:44 -0800

    Get by role

--- react-testing-library/src/App.test.js
+++ react-testing-library/src/App.test.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
 
-describe('renders App component', () => {
-
+describe("renders App component", () => {
   // test('Displays html', () => {
   //   render(<App />);
   //   screen.debug()
   // });
 
-  test('Exact match of string without last character', () => {
+  // test('Exact match of string without last character', () => {
+  //   render(<App />);
+  //   expect(screen.getByText('Search')).toBeInTheDocument()
+  // }); // Fails
+
+  test("Exact match of string", () => {
     render(<App />);
-    expect(screen.getByText('Search')).toBeInTheDocument()
+    expect(screen.getByText("Search:")).toBeInTheDocument();
   });
-  
-  test('Exact match of string', () => {
-    render(<App />);
-    expect(screen.getByText('Search:')).toBeInTheDocument()
-  });
-  
+
   // Whereas a string argument is used for the exact match, a regular expression can be used for a partial match which is often more convenient
-  test('Partial string match using regex', () => {
+  test("Partial string match using regex", () => {
     render(<App />);
-    expect(screen.getAllByText(/Search/)).toHaveLength(2)
+    expect(screen.getAllByText(/Search/)).toHaveLength(2);
+  });
+
+  // Use this to find out what the role options are
+  // test('Shows all role options', () => {
+  //   render(<App />);
+  //   screen.getByRole('');
+  // })
+
+  test("Exists an input element (by role)", () => {
+    render(<App />);
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 });
